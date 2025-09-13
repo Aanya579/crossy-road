99a2f87af98dcd647d3077b3268e4f70d78f4257
Aanya579 <aanyaprakash65@gmail.com>
2025-09-13 11:19:51 -0700

    Add files via upload

---
+++ crossy_road.py
@@ -0,0 +1,73 @@
+from turtle import *
+screen = Screen()
+bg = Turtle()
+player = Turtle()
+car = Turtle()
+screen.register_shape("car.gif")
+car.shape("car.gif")
+car.color("red")
+car.resizemode("user")
+car.shapesize(0.1, 0.1)
+bg.left(90)
+screen.register_shape("road.gif")
+bg.shape("road.gif")
+player.left(90)
+screen.addshape("chicken.gif")
+player.shape("chicken.gif")
+player.speed(0)
+def move_up():
+  pos = player.pos()
+  player.goto(pos[0], pos[1]+10)
+def move_down():
+  pos = player.pos()
+  player.goto(pos[0], pos[1]-10)
+def move_right():
+  pos = player.pos()
+  player.goto(pos[0]+10, pos[1])
+def move_left():
+  pos = player.pos()
+  player.goto(pos[0]-10, pos[1])
+  
+screen.onkey(move_left, "Left")
+screen.onkey(move_right, "Right")
+screen.onkey(move_down, "Down")
+screen.onkey(move_up, "Up")
+screen.listen()
+  
+def checkAABBCollision(A, B):
+
+  AisToTheRightOfB = A.position()[0] - 30 > B.position()[0] - 30
+  AisToTheLeftOfB = A.position()[0] + 30 < B.position()[0] + 30
+  AisAboveB = A.position()[1] - 30 < B.position()[1] - 30
+  AisBelowB = A.position()[1] + 30 > B.position()[1] + 30
+  return not (AisToTheRightOfB or AisToTheLeftOfB or AisAboveB or AisBelowB);
+  
+class storage:
+  def __init__(self, x, y):
+    self.objects = []
+    self.x = -300
+    self.y = 150
+    
+lane1 = storage(0, 0)
+lane1.objects.append(car)
+
+timer = 10
+score = 0
+lives = 3
+
+while True:
+  for o in lane1.objects:
+    if checkAABBCollision(o, player):
+      lives -= 1
+  for l in lane1.objects:
+    l.forward(3)
+  timer -= 1
+  if timer == 0:
+    car = Turtle()
+    car.shape("square")
+    car.color("red")
+    car.teleport(lane1.x, lane1.y)
+    lane1.objects.append(car)
+    timer = 10
+  if lives == 0:
+   print("Game over")
