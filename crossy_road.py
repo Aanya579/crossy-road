5d44ca566ba993da439019fb539979a450793763
Aanya <aanyaprakash65@gmail.com>
2025-09-27 12:02:22 -0700

    updated reset and exit function and collision

--- crossy_road.py
+++ crossy_road.py
@@ -40,11 +40,7 @@ screen.listen()
   
 def checkAABBCollision(A, B):
 
-  AisToTheRightOfB = A.position()[0] - 30 > B.position()[0] - 30
-  AisToTheLeftOfB = A.position()[0] + 30 < B.position()[0] + 30
-  AisAboveB = A.position()[1] - 30 < B.position()[1] - 30
-  AisBelowB = A.position()[1] + 30 > B.position()[1] + 30
-  return not (AisToTheRightOfB or AisToTheLeftOfB or AisAboveB or AisBelowB);
+  return A.xcor() < B.xcor() + 50 and A.xcor() + 55 > B.xcor() and A.ycor() < B.ycor() + 50 and A.ycor() + 55 > B.ycor()
   
 class storage:
   def __init__(self, x, y):
@@ -67,9 +63,20 @@ def  reset():
   lives = 3
   global timer
   timer = 30
+  line1.clear()
+  line2.clear()
+  for turtle in lane1.objects:
+    turtle.ht()
+    turtle.clear()
+  lane1.objects = []
+
+def exit():
+  screen.bye()
 
 line1 = Turtle()
 line2 = Turtle()
+
+has_drawn_text = False
 
 while True:
   for o in lane1.objects:
@@ -91,10 +98,16 @@ while True:
     lane1.objects.append(car)
     time = random.randint(30, 100)
     timer = time
-    lives -= 1
-  if lives == 0:
+  if lives == 0 and has_drawn_text == False:
+   has_drawn_text = True
    timer = 100000000000000
+   line1.penup()
+   line2.penup()
+   line1.goto(-50,-200)
+   line2.goto(-200, -250)
+   line1.pendown()
+   line2.pendown()
    line1.write("Game over")
    line2.write("Would you like to play again? Press r to start again, press n to end the game.")
   screen.onkeypress(reset, "r")
-  screen.onkeypress(sys.exit, "n")
+  screen.onkeypress(exit, "n")
