870b51ddc6948ba0d31d41ae5514c6b426c32861
Aanya <aanyaprakash65@gmail.com>
2025-10-11 12:01:14 -0700

    updated collision and lane2

--- crossy_road.py
+++ crossy_road.py
@@ -5,11 +5,16 @@ screen = Screen()
 bg = Turtle()
 player = Turtle()
 car = Turtle()
+car2 = Turtle()
 screen.register_shape("car.gif")
 car.shape("car.gif")
 car.color("red")
 car.resizemode("user")
-car.shapesize(0.00005, 0.00005)
+car2.shapesize(0.00005, 0.00005)
+car2.shape("car.gif")
+car2.color("red")
+car2.resizemode("user")
+car2.shapesize(0.00005, 0.00005)
 bg.left(90)
 screen.register_shape("road.gif")
 bg.shape("road.gif")
@@ -44,18 +49,20 @@ screen.onkey(move_down, "Down")
 screen.onkey(move_up, "Up")
 screen.listen()
   
-def checkAABBCollision(A, B):
+def checkAABBCollision(B, A):
 
-  return A.xcor() < B.xcor() + 50 and A.xcor() + 55 > B.xcor() and A.ycor() < B.ycor() + 50 and A.ycor() + 55 > B.ycor()
+  return A.xcor() < B.xcor() + 180 and A.xcor() + 55 > B.xcor() and A.ycor() < B.ycor() + 90 and A.ycor() + 55 > B.ycor()
   
 class storage:
   def __init__(self, x, y):
     self.objects = []
-    self.x = -300
-    self.y = 150
+    self.x = x
+    self.y = y
     
-lane1 = storage(0, 0)
+lane1 = storage(-300, 150)
 lane1.objects.append(car)
+lane2 = storage(200, -50)
+lane2.objects.append(car)
 
 timer = 30
 score = 0
@@ -81,7 +88,6 @@ def  reset():
   car.color("red")
   car.resizemode("user")
   car.shapesize(0.00005, 0.00005)
-
   bg = Turtle()
   bg.left(90)
   bg.shape("road.gif")
@@ -97,6 +103,8 @@ def  reset():
   screen.onkey(move_down, "Down")
   screen.onkey(move_up, "Up")
   screen.listen()
+
+print(screen.screensize())
 
 def exit():
   screen.bye()
@@ -118,6 +126,21 @@ def game():
           print("lives", lives)
     for l in lane1.objects:
       l.forward(7)
+      if l.xcor() > 200:
+        l.ht()
+        del l
+    for o in lane2.objects:
+      if checkAABBCollision(o, player):
+        if not o in cars:
+          cars.append(o)
+          lives -= 1
+          print("lives", lives)
+    for l in lane2.objects:
+      l.forward(-7)
+      if l.xcor() < -200:
+        l.ht()
+        del l
+
     timer -= 1
     score += 10
 
@@ -128,7 +151,14 @@ def game():
       car.resizemode("user")
       car.shapesize(0.00005, 0.000005)
       car.teleport(lane1.x, lane1.y)
+      car2 = Turtle()
+      car2.shape("car.gif")
+      car2.color("red")
+      car2.resizemode("user")
+      car2.shapesize(0.00005, 0.000005)
+      car2.teleport(lane2.x, lane2.y)
       lane1.objects.append(car)
+      lane2.objects.append(car2)
       time = random.randint(30, 100)
       timer = time
       
@@ -146,5 +176,8 @@ def game():
       line2.write("Would you like to play again? Press r to start again, press n to end the game.")
     screen.onkeypress(reset, "r")
     screen.onkeypress(exit, "n")
+
+    if player.ycor() > 240:
+      print("You win!")
 
 game()
